0aee50458e89c864b03b4a9ce90bd54a5af4fe69
showlotus <2257030399@qq.com>
2023-12-21 22:36:27 +0800

    chore: add dist

--- packages/jsx-explorer/vite.config.ts
+++ packages/jsx-explorer/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@vue/babel-plugin-jsx': '@vue/babel-plugin-jsx/src/index.ts',
